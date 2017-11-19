7db93ed8ff18dbdfe3a7659ae9cdf4e4f496fe89
baitu026 <32809769+baitu026@users.noreply.github.com>
2017-11-19 21:56:29 +0800

    Add files via upload

---
+++ Special Effect/CSS3PhotoWall/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="style.css" type="text/css"  rel="stylesheet"/>          <!--链接到css样式-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+<h1>我的照片墙</h1>
+
+<div class='container'>
+    <img class='img1' src="images/img1.jpg">
+    <img class='img2' src="images/img2.jpg">
+    <img class='img3' src="images/img3.jpg">
+    <img class='img4' src="images/img4.jpg">
+    <img class='img5' src="images/img5.jpg">
+    <img class='img6' src="images/img6.jpg">
+    <img class='img7' src="images/img7.jpg">
+    <img class='img8' src="images/img8.jpg">
+    <img class='img9' src="images/img9.jpg">
+    <img class='img0' src="images/img0.jpg">
+
+</div>
+</body>
+</html>
